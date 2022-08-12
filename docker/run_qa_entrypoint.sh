26dd8f979b069f4c128f2ceee2594595ae37123a
Wang, Yi <yi.a.wang@intel.com>
2022-08-12 15:11:02 +0800

    add "-bootstrap ssh" (#19)
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- docker/run_qa_entrypoint.sh
+++ docker/run_qa_entrypoint.sh
@@ -16,7 +16,7 @@ if [ $master_node = True ];then
   # Start training BERT
   run_file="transformers/examples/pytorch/question-answering/run_qa.py"
 
-  mpirun -f ./tmp/hostfile -np ${mpi_n} -ppn ${mpi_ppn} -genv OMP_NUM_THREADS=${omp_num_threads} python3 ${run_file} --model_name_or_path ${model_name} --dataset_name ${dataset_name}   --do_train  --do_eval  --per_device_train_batch_size ${per_device_train_batch_size} --learning_rate ${learning_rate} --num_train_epochs ${num_train_epochs} --max_seq_length ${max_seq_length} --doc_stride ${doc_stride} --output_dir ${output_dir}  --no_cuda   --xpu_backend ${xpu_backend} --dataloader_pin_memory ${dataloader_pin_memory} --use_ipex ${use_ipex} --bf16 ${bf16} --overwrite_output_dir
+  mpirun -bootstrap ssh -f ./tmp/hostfile -np ${mpi_n} -ppn ${mpi_ppn} -genv OMP_NUM_THREADS=${omp_num_threads} python3 ${run_file} --model_name_or_path ${model_name} --dataset_name ${dataset_name}   --do_train  --do_eval  --per_device_train_batch_size ${per_device_train_batch_size} --learning_rate ${learning_rate} --num_train_epochs ${num_train_epochs} --max_seq_length ${max_seq_length} --doc_stride ${doc_stride} --output_dir ${output_dir}  --no_cuda   --xpu_backend ${xpu_backend} --dataloader_pin_memory ${dataloader_pin_memory} --use_ipex ${use_ipex} --bf16 ${bf16} --overwrite_output_dir
 else
   while true
   do
