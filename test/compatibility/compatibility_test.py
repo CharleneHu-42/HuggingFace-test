19ee001c9643d38ae5289ac124f7d321ddda7998
Wang, Yi <yi.a.wang@intel.com>
2022-10-21 09:13:24 +0800

    add optimum-intel test (#28)
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- test/compatibility/compatibility_test.py
+++ test/compatibility/compatibility_test.py
@@ -2,47 +2,111 @@ import argparse
 import csv
 import json
 import logging
+import os
+
 import testcase
 
 logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
-        level=logging.INFO,
-        datefmt="[%X]",
-    )
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+    level=logging.INFO,
+    datefmt="[%X]",
+)
 logger = logging.getLogger(__name__)
+
+
 def init_arguments_parser():
-    parser  = argparse.ArgumentParser(description="compatiblity test")
-    parser.add_argument('-o', '--output', default='/tmp', help="Assign output folder, default is /tmp")
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
-    parser.add_argument('-c', '--case', default="text-classification-mrpc", choices=["text-classification-mrpc","text-classification-sst2"],  
-                        help="choice test case")
-    parser.add_argument('-m', "--model", default='bert-base-cased', help="Assign model name, default is bert-base-cased")
+    parser = argparse.ArgumentParser(description="compatiblity test")
+    parser.add_argument(
+        "-o", "--output", default="/tmp", help="Assign output folder, default is /tmp"
+    )
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
+    parser.add_argument(
+        "-c",
+        "--case",
+        default="text-classification-mrpc",
+        choices=["text-classification-mrpc", "text-classification-sst2"],
+        help="choice test case",
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        default="bert-base-cased",
+        help="Assign model name, default is bert-base-cased",
+    )
+    parser.add_argument(
+        "--finetune_dir",
+        default="/home/wangyi/project/hugface/transformers/examples/pytorch/text-classification",
+        help="huggingface finetune examples dir",
+    )
+    parser.add_argument(
+        "--optimum_dir",
+        default="/home/wangyi/project/hugface/optimum-intel/examples/neural_compressor/text-classification",
+        help="Huggingface optimum-intel examples dir",
+    )
     return parser
 
-def get_case_command(arg: argparse.Namespace):
+
+def get_finetune_case_command(arg: argparse.Namespace):
     switch = {
-        "text-classification-mrpc": "examples/pytorch/text-classification/run_glue.py --task_name mrpc --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 8 --no_cuda --num_train_epochs 1 --overwrite_output_dir True",
-        "text-classification-sst2": "examples/pytorch/text-classification/run_glue.py --task_name sst2 --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 8 --no_cuda --num_train_epochs 1 --overwrite_output_dir True",
+        "text-classification-mrpc": "run_glue.py --task_name mrpc --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 8 --no_cuda --num_train_epochs 1 --overwrite_output_dir",
+        "text-classification-sst2": "run_glue.py --task_name sst2 --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 8 --no_cuda --num_train_epochs 1 --overwrite_output_dir",
     }
     return switch[arg.case]
+
+
+def get_optimum_case_command(arg: argparse.Namespace):
+    switch = {
+        "text-classification-mrpc": "run_glue.py --task_name mrpc --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 8 --no_cuda --num_train_epochs 1 --overwrite_output_dir --verify_loading",
+        "text-classification-sst2": "run_glue.py --task_name sst2 --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 8 --no_cuda --num_train_epochs 1 --overwrite_output_dir --verify_loading",
+    }
+    return switch[arg.case]
+
 
 def main():
     parser = init_arguments_parser()
     args = parser.parse_args()
-    f = open(args.output + "/summery.csv", mode="w", encoding="utf-8", newline='')
-    csv_writer = csv.DictWriter(f,fieldnames=["casename","optimum-intel","subname","eval_f1","eval_samples_per_second","train_samples_per_second","train_loss"])
+    f = open(args.output + "/summery.csv", mode="w", encoding="utf-8", newline="")
+    csv_writer = csv.DictWriter(
+        f,
+        fieldnames=[
+            "casename",
+            "optimum-intel",
+            "subname",
+            "eval_f1",
+            "eval_samples_per_second",
+            "train_samples_per_second",
+            "train_loss",
+        ],
+    )
     csv_writer.writeheader()
-    base_cmd = get_case_command(args)
-    testcase.run_ipex_bf16_train_evaluate(args.case, base_cmd, args, csv_writer)
-    testcase.run_ipex_fp32_train_evaluate(args.case, base_cmd, args, csv_writer)
-    testcase.run_torch_bf16_train_evaluate(args.case, base_cmd, args, csv_writer)
-    testcase.run_torch_fp32_train_evaluate(args.case, base_cmd, args, csv_writer)
-    
-    
+    finetune_base_cmd = get_finetune_case_command(args)
+    optimum_base_cmd = get_optimum_case_command(args)
+    testcase.run_ipex_bf16_finetune_evaluate(
+        args.case, finetune_base_cmd, args, csv_writer
+    )
+    testcase.run_ipex_fp32_finetune_evaluate(
+        args.case, finetune_base_cmd, args, csv_writer
+    )
+    testcase.run_torch_bf16_finetune_evaluate(
+        args.case, finetune_base_cmd, args, csv_writer
+    )
+    testcase.run_torch_fp32_finetune_evaluate(
+        args.case, finetune_base_cmd, args, csv_writer
+    )
+
+    testcase.run_ipex_bf16_finetune_quantization(
+        args.case, finetune_base_cmd, optimum_base_cmd, args, csv_writer
+    )
+    testcase.run_ipex_fp32_finetune_quantization(
+        args.case, finetune_base_cmd, optimum_base_cmd, args, csv_writer
+    )
+    testcase.run_torch_bf16_finetune_quantization(
+        args.case, finetune_base_cmd, optimum_base_cmd, args, csv_writer
+    )
+    testcase.run_torch_fp32_finetune_quantization(
+        args.case, finetune_base_cmd, optimum_base_cmd, args, csv_writer
+    )
+
 
 if __name__ == "__main__":
     main()
-
-
-
-  
--- test/compatibility/testcase.py
+++ test/compatibility/testcase.py
@@ -1,14 +1,16 @@
-import logging
-import csv
 import argparse
-import subprocess
 import copy
+import csv
+import logging
 import os
+import subprocess
 
 logger = logging.getLogger(__name__)
 
-def get_json_map(path:str):
-    import json 
+
+def get_json_map(path: str):
+    import json
+
     results = {}
     if os.path.exists(path):
         with open(path, "r") as f:
@@ -17,9 +19,24 @@ def get_json_map(path:str):
         raise ValueError(f"can't find {path}")
     return results
 
-def run_finetune(case_name:str, subname:str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter):
+
+def run_finetune(
+    case_name: str,
+    subname: str,
+    basecmd: str,
+    args: argparse.Namespace,
+    csv_writer: csv.DictWriter,
+):
+    os.chdir(args.finetune_dir)
     output_dir = args.output + "/" + case_name + "-" + subname
-    cmdstr = basecmd + " --output_dir " + output_dir + " --model_name_or_path " + args.model +" --do_eval --do_train"
+    cmdstr = (
+        basecmd
+        + " --output_dir "
+        + output_dir
+        + " --model_name_or_path "
+        + args.model
+        + " --do_eval --do_train"
+    )
     if "bf16" in subname:
         cmdstr += " --bf16"
     if "ipex" in subname:
@@ -34,10 +51,10 @@ def run_finetune(case_name:str, subname:str, basecmd: str, args: argparse.Namesp
         "eval_f1": None,
         "eval_samples_per_second": None,
         "train_samples_per_second": None,
-        "train_loss": None
+        "train_loss": None,
     }
     a["casename"] = case_name
-    a["subname"] =  subname + "-finetune"
+    a["subname"] = subname + "-finetune"
     if exit == 0:
         results = get_json_map(output_dir + "/all_results.json")
         a["eval_f1"] = results["eval_f1"]
@@ -47,10 +64,26 @@ def run_finetune(case_name:str, subname:str, basecmd: str, args: argparse.Namesp
     csv_writer.writerow(a)
     return
 
-def run_inference(case_name:str, subname:str, inf_subname:str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter):
+
+def run_inference(
+    case_name: str,
+    subname: str,
+    inf_subname: str,
+    basecmd: str,
+    args: argparse.Namespace,
+    csv_writer: csv.DictWriter,
+):
+    os.chdir(args.finetune_dir)
     model_dir = args.output + "/" + case_name + "-" + subname
-    output_dir = args.output + "/" + case_name + "-" + subname + "/"+ inf_subname
-    cmdstr = basecmd + " --output_dir " + output_dir + " --model_name_or_path " + model_dir + " --do_eval"
+    output_dir = args.output + "/" + case_name + "-" + subname + "/" + inf_subname
+    cmdstr = (
+        basecmd
+        + " --output_dir "
+        + output_dir
+        + " --model_name_or_path "
+        + model_dir
+        + " --do_eval"
+    )
     if "bf16" in inf_subname:
         cmdstr += " --bf16"
     if "ipex" in inf_subname:
@@ -65,53 +98,205 @@ def run_inference(case_name:str, subname:str, inf_subname:str, basecmd: str, arg
         "eval_f1": None,
         "eval_samples_per_second": None,
         "train_samples_per_second": None,
-        "train_loss": None
+        "train_loss": None,
     }
     a["casename"] = case_name
-    a["subname"] =  subname + "-finetune-model-inf-only-" + inf_subname
+    a["subname"] = subname + "-finetune-model-inf-only-" + inf_subname
     if exit == 0:
         results = get_json_map(output_dir + "/eval_results.json")
         a["eval_f1"] = results["eval_f1"]
         a["eval_samples_per_second"] = results["eval_samples_per_second"]
     csv_writer.writerow(a)
     return
- 
-def run_ipex_bf16_train_evaluate(case_name:str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter):
-    #step1: finetune to get the model firstly
+
+
+def run_optimum_intel_quantization(
+    case_name: str,
+    subname: str,
+    qt_subname: str,
+    basecmd: str,
+    args: argparse.Namespace,
+    csv_writer: csv.DictWriter,
+):
+    os.chdir(args.optimum_dir)
+    model_dir = args.output + "/" + case_name + "-" + subname
+    output_dir = args.output + "/" + case_name + "-" + subname + "/ptq-" + qt_subname
+    cmdstr = (
+        basecmd
+        + " --output_dir "
+        + output_dir
+        + " --model_name_or_path "
+        + model_dir
+        + " --do_eval --do_train"
+    )
+    if "bf16" in qt_subname:
+        cmdstr += " --bf16"
+    if "ipex" in qt_subname:
+        cmdstr += " --use_ipex"
+    if "static" in qt_subname:
+        cmdstr += " --apply_quantization --quantization_approach static"
+    if "dynamic" in qt_subname:
+        cmdstr += " --apply_quantization --quantization_approach dynamic"
+    if "aware_training" in qt_subname:
+        cmdstr += " --apply_quantization --quantization_approach aware_training"
+
+    cmdstr = "taskset -c 0-3 python3 " + cmdstr
+    logger.info(f"{cmdstr}")
+    exit, output = subprocess.getstatusoutput(cmdstr)
+    a = {
+        "optimum-intel": "w",
+        "casename": None,
+        "subname": None,
+        "eval_f1": None,
+        "eval_samples_per_second": None,
+        "train_samples_per_second": None,
+        "train_loss": None,
+    }
+    a["casename"] = case_name
+    a["subname"] = subname + "-finetune-model-only-qt-" + qt_subname
+    if exit == 0:
+        results = get_json_map(output_dir + "/eval_results.json")
+        a["eval_f1"] = results["eval_f1"]
+        a["eval_samples_per_second"] = results["eval_samples_per_second"]
+    csv_writer.writerow(a)
+    return
+
+
+def run_ipex_bf16_finetune_evaluate(
+    case_name: str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter
+):
+    # step1: finetune to get the model firstly
     run_finetune(case_name, "ipex-bf16", basecmd, args, csv_writer)
-    #step2: run bf16 or fp32 inference on the finetune output model
+    # step2: run bf16 or fp32 inference on the finetune output model
     run_inference(case_name, "ipex-bf16", "ipex-bf16", basecmd, args, csv_writer)
     run_inference(case_name, "ipex-bf16", "ipex-fp32", basecmd, args, csv_writer)
     run_inference(case_name, "ipex-bf16", "fp32", basecmd, args, csv_writer)
     run_inference(case_name, "ipex-bf16", "bf16", basecmd, args, csv_writer)
     return
 
-def run_ipex_fp32_train_evaluate(case_name:str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter):
-    #step1: finetune to get the model firstly
+
+def run_ipex_fp32_finetune_evaluate(
+    case_name: str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter
+):
+    # step1: finetune to get the model firstly
     run_finetune(case_name, "ipex-fp32", basecmd, args, csv_writer)
-    #step2: run bf16 or fp32 inference on the finetune output model
+    # step2: run bf16 or fp32 inference on the finetune output model
     run_inference(case_name, "ipex-fp32", "ipex-bf16", basecmd, args, csv_writer)
     run_inference(case_name, "ipex-fp32", "ipex-fp32", basecmd, args, csv_writer)
     run_inference(case_name, "ipex-fp32", "fp32", basecmd, args, csv_writer)
     run_inference(case_name, "ipex-fp32", "bf16", basecmd, args, csv_writer)
     return
 
-def run_torch_bf16_train_evaluate(case_name:str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter):
-    #step1: finetune to get the model firstly
+
+def run_torch_bf16_finetune_evaluate(
+    case_name: str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter
+):
+    # step1: finetune to get the model firstly
     run_finetune(case_name, "bf16", basecmd, args, csv_writer)
-    #step2: run bf16 or fp32 inference on the finetune output model
+    # step2: run bf16 or fp32 inference on the finetune output model
     run_inference(case_name, "bf16", "ipex-bf16", basecmd, args, csv_writer)
     run_inference(case_name, "bf16", "ipex-fp32", basecmd, args, csv_writer)
     run_inference(case_name, "bf16", "fp32", basecmd, args, csv_writer)
     run_inference(case_name, "bf16", "bf16", basecmd, args, csv_writer)
     return
 
-def run_torch_fp32_train_evaluate(case_name:str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter):
-    #step1: finetune to get the model firstly
+
+def run_torch_fp32_finetune_evaluate(
+    case_name: str, basecmd: str, args: argparse.Namespace, csv_writer: csv.DictWriter
+):
+    # step1: finetune to get the model firstly
     run_finetune(case_name, "fp32", basecmd, args, csv_writer)
-    #step2: run bf16 or fp32 inference on the finetune output model
+    # step2: run bf16 or fp32 inference on the finetune output model
     run_inference(case_name, "fp32", "ipex-bf16", basecmd, args, csv_writer)
     run_inference(case_name, "fp32", "ipex-fp32", basecmd, args, csv_writer)
     run_inference(case_name, "fp32", "fp32", basecmd, args, csv_writer)
     run_inference(case_name, "fp32", "bf16", basecmd, args, csv_writer)
+    return
+
+
+def run_ipex_bf16_finetune_quantization(
+    case_name: str,
+    finetune_cmd: str,
+    optimum_cmd: str,
+    args: argparse.Namespace,
+    csv_writer: csv.DictWriter,
+):
+    # step1: finetune to get the model firstly
+    run_finetune(case_name, "ipex-bf16-1", finetune_cmd, args, csv_writer)
+    # step2: quantization using optimum-intel
+    run_optimum_intel_quantization(
+        case_name, "ipex-bf16-1", "static", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "ipex-bf16-1", "dynamic", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "ipex-bf16-1", "aware_training", optimum_cmd, args, csv_writer
+    )
+    return
+
+
+def run_ipex_fp32_finetune_quantization(
+    case_name: str,
+    finetune_cmd: str,
+    optimum_cmd: str,
+    args: argparse.Namespace,
+    csv_writer: csv.DictWriter,
+):
+    # step1: finetune to get the model firstly
+    run_finetune(case_name, "ipex-fp32-1", finetune_cmd, args, csv_writer)
+    # step2: quantization using optimum-intel
+    run_optimum_intel_quantization(
+        case_name, "ipex-fp32-1", "static", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "ipex-fp32-1", "dynamic", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "ipex-fp32-1", "aware_training", optimum_cmd, args, csv_writer
+    )
+    return
+
+
+def run_torch_bf16_finetune_quantization(
+    case_name: str,
+    finetune_cmd: str,
+    optimum_cmd: str,
+    args: argparse.Namespace,
+    csv_writer: csv.DictWriter,
+):
+    # step1: finetune to get the model firstly
+    run_finetune(case_name, "bf16-1", finetune_cmd, args, csv_writer)
+    # step2: quantization using optimum-intel
+    run_optimum_intel_quantization(
+        case_name, "bf16-1", "static", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "bf16-1", "dynamic", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "bf16-1", "aware_training", optimum_cmd, args, csv_writer
+    )
+    return
+
+
+def run_torch_fp32_finetune_quantization(
+    case_name: str,
+    finetune_cmd: str,
+    optimum_cmd: str,
+    args: argparse.Namespace,
+    csv_writer: csv.DictWriter,
+):
+    # step1: finetune to get the model firstly
+    run_finetune(case_name, "fp32-1", finetune_cmd, args, csv_writer)
+    # step2: quantization using optimum-intel
+    run_optimum_intel_quantization(
+        case_name, "fp32-1", "static", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "fp32-1", "dynamic", optimum_cmd, args, csv_writer
+    )
+    run_optimum_intel_quantization(
+        case_name, "fp32-1", "aware_training", optimum_cmd, args, csv_writer
+    )
     return
