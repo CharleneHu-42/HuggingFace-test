6fe1a18fdd0c0a8b0d325bc274a3a10edbecfa27
Wang, Yi <yi.a.wang@intel.com>
2022-12-19 16:28:29 +0800

    add memory/cpu_percent gather script for accelerate launch case which could not use ddp_memory_test.py (#38)
    could capture process with "key info" in yaml

--- test/memory/ddp_accelerate_info_gather.py
+++ test/memory/ddp_accelerate_info_gather.py
@@ -41,23 +41,24 @@ def init_arguments_parser():
     return parser
 
 
-def collect_memory(eval_pid: int, name: str, output: str, deadline):
+def collect_memory(name: str, cmd: str, output: str, deadline):
     import csv
 
     import psutil
 
     os.environ["RANK"] = str(os.environ.get("PMI_RANK", 0))
     os.environ["WORLD_SIZE"] = str(os.environ.get("PMI_SIZE", 1))
+    os.environ["MASTER_PORT"] = str(int(os.environ.get("MASTER_PORT", "29500")) + 1)
     backend = "ccl"
     dist.init_process_group(backend)
 
     if dist.get_rank() == 0:
         f = open(output + name + ".csv", mode="w", encoding="utf-8", newline="")
-
         csv_writer = csv.DictWriter(
             f,
             fieldnames=[
                 "rank",
+                "pid",
                 "memory-rss",
                 "memory-vms",
                 "memory-data",
@@ -71,43 +72,41 @@ def collect_memory(eval_pid: int, name: str, output: str, deadline):
     while find and time.time() < deadline:
         find = False
         for p in psutil.process_iter():
-            if p.pid == eval_pid:
+            if len(p.cmdline()) > 1 and cmd in (p.cmdline()[1]):
                 a = {
                     "rank": dist.get_rank(),
-                    "memory-rss": p.memory_info().rss,
-                    "memory-vms": p.memory_info().vms,
-                    "memory-data": p.memory_info().data,
+                    "pid": p.pid,
+                    "memory-rss": p.memory_info().rss * 1.0 / 1024 / 1024 / 1024,
+                    "memory-vms": p.memory_info().vms * 1.0 / 1024 / 1024 / 1024,
+                    "memory-data": p.memory_info().data * 1.0 / 1024 / 1024 / 1024,
                     "cpu-percent": p.cpu_percent(),
                 }
-                output = [None for _ in range(dist.get_world_size())]
-                dist.gather_object(
-                    a,
-                    output if dist.get_rank() == 0 else None,
-                    dst=0,
-                )
-                if dist.get_rank() == 0:
-                    for item in output:
-                        csv_writer.writerow(item)
+                logger.info(f"{a}")
+                if dist.get_world_size() > 1:
+                    output = [None for _ in range(dist.get_world_size())]
+                    dist.gather_object(
+                        a,
+                        output if dist.get_rank() == 0 else None,
+                        dst=0,
+                    )
+                    if dist.get_rank() == 0:
+                        for item in output:
+                            csv_writer.writerow(item)
+                else:
+                    csv_writer.writerow(a)
                 find = True
-                time.sleep(1)
+        time.sleep(1)
     if dist.get_rank() == 0:
         f.close()
-    dist.barrier()
+
+    if dist.get_world_size() > 1:
+        dist.barrier()
 
 
 def run_and_collect_memory(case: dict, arg: argparse.Namespace):
-    current_env = os.environ.copy()
-    current_env["MASTER_PORT"] = str(int(os.environ.get("MASTER_PORT", "29500")) + 1)
-    eval_p = subprocess.Popen(
-        re.split("\s+", case["cmd"]), cwd=case["work_dir"], env=current_env
-    )
-    time.sleep(case["warmup"])
     deadline = time.time() + case["timeout"]
     logger.info("start collect_memory process")
-    collect_memory(eval_p.pid, case["title"], arg.output, deadline)
-    if eval_p.poll() is None:
-        eval_p.kill()
-    eval_p.wait()
+    collect_memory(case["title"], case["cmd"], arg.output, deadline)
     return
 
 
