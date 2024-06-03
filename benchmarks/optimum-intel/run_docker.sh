d110e05c466fda165b70333e6cdd99a234203468
Fanli Lin <fanli.lin@intel.com>
2024-06-03 07:39:10 -0700

    add xpu Docker

--- benchmarks/optimum-intel/run_docker.sh
+++ benchmarks/optimum-intel/run_docker.sh
@@ -8,19 +8,31 @@ PROJECT_ROOT=$(dirname $(dirname $(dirname $(dirname $SCRIPT))))
 
 if [[ "$eval_mode" == "trt-llm" ]]; then 
 	tag=trt-llm
+	docker run -it \
+		-e http_proxy=${http_proxy} \
+		-e https_proxy=${https_proxy} \
+		-e no_proxy=${no_proxy} \
+		-v $hf_cache:/root/.cache \
+		-v $PROJECT_ROOT:/mnt/code \
+		-w /mnt/code/HuggingFace/benchmarks/optimum-intel \
+		--runtime=nvidia \
+		--gpus all \
+		--entrypoint /bin/bash \
+		--name $tag \
+		benchmark/mmlu:$tag
 else 
 	tag=opt-intel
+	docker run -it \
+		--privileged  \
+		-e http_proxy=${http_proxy} \
+		-e https_proxy=${https_proxy} \
+		-e no_proxy=${no_proxy} \
+		-v $hf_cache:/root/.cache \
+		-v $PROJECT_ROOT:/mnt/code \
+		-w /mnt/code/HuggingFace/benchmarks/optimum-intel \
+		--device=/dev/dri \
+		--ipc=host \
+		--entrypoint /bin/bash \
+		--name $tag \
+		benchmark/mmlu:$tag
 fi 
-
-docker run -it \
-	-e http_proxy=${http_proxy} \
-	-e https_proxy=${https_proxy} \
-	-e no_proxy=${no_proxy} \
-	-v $hf_cache:/root/.cache \
-	-v $PROJECT_ROOT:/mnt/code \
-	-w /mnt/code/HuggingFace/benchmarks/optimum-intel \
-	--runtime=nvidia \
-	--gpus all \
-	--entrypoint /bin/bash \
-	--name $tag \
-	benchmark/mmlu:$tag
