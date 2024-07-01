c94b6442b605203cbee21657139a20d935d15284
Fanli Lin <fanli.lin@intel.com>
2024-07-01 05:57:02 -0700

    revert

--- tests/workloads/fine-tune/run_fine-tune.py
+++ tests/workloads/fine-tune/run_fine-tune.py
@@ -258,7 +258,7 @@ def train(
             warmup_steps=100,
             num_train_epochs=num_epochs,
             learning_rate=learning_rate,
-            logging_steps=1,
+            logging_steps=10,
             optim="adamw_torch",
             eval_strategy="steps" if val_set_size > 0 else "no",
             save_strategy="steps",
