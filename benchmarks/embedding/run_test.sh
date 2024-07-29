3578238bbcac992f6974d066b87bf3020277ae23
Daniel Huang <daniel1.huang@intel.com>
2024-07-29 09:29:30 -0700

    Use for loop

--- benchmarks/embedding/run_test.sh
+++ benchmarks/embedding/run_test.sh
@@ -1,17 +1,16 @@
+#!/bin/bash
+
 if [[ $# -ne 1 ]]; then
 	echo "Usage: ./run_test.sh <MODEL ID>"
-    exit 1
+	exit 1
 fi
 MODEL=$1
 
+echo "python test_sync.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 "
 python test_sync.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 1
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 2
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 4
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 8
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 16
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 32
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 64
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 128
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 256
-python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size 512
+
+for ((i = 0; i < 10; i++)); do
+	batch_size=$((2 ** $i))
+	echo "python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size $batch_size"
+	python test_async.py --model $MODEL --port 8081 --endpoint /embed --max_length=512 --save-result --result-dir ../../../hf-benchmarks --num-prompts 5120 --batch_size $batch_size
+done
