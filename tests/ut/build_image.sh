80e089bbf0804cd5000c79d7c7c86282340ad0c3
Lin, Fanli <fanli.lin@intel.com>
2024-11-17 18:59:06 -0800

    update silent installation mode

--- tests/ut/build_image.sh
+++ tests/ut/build_image.sh
@@ -50,5 +50,4 @@ docker build \
 	-f Dockerfile.${device} . \
 	--build-arg http_proxy=${http_proxy} \
 	--build-arg https_proxy=${https_proxy} \
-	--build-arg no_proxy=${no_proxy} \
 	-t huggingface-ut/${device}
