6013f12cadee1ba5303cdcae08c2d580bd89406b
Yao, Matrix <matrix.yao@intel.com>
2024-12-03 17:51:34 -0800

    make script clean and coherent
    Signed-off-by: Yao, Matrix <matrix.yao@intel.com>

--- tests/workloads/run.sh
+++ tests/workloads/run.sh
@@ -1,54 +1,60 @@
 #!/bin/bash
 
 # Default variable values
-ipex_optimize=False
-jit=False
-torch_compile=False
+
+## test case configurations
 task_name=""
 model_id=""
 model_dtype="float32"
 autocast_dtype="float32"
-backend="inductor"
-device="cpu"
 batch_size=1
 num_beams=4
 input_tokens=32
 output_tokens=32
-ipex_optimize_transformers="False"
-gradient_checkpointing="False"
-num_processes=4
-warm_up_steps=10
-run_steps=10
-optimum_intel="False"
+gradient_checkpointing=False
 quant_algo="None"
 quant_dtype="None"
 
-# Function to display script usage
+## runtime configurations
+backend="inductor"
+device="cpu"
+num_processes=4
+warm_up_steps=10
+run_steps=10
+
+## optimization options
+ipex_optimize=False
+ipex_optimize_transformers=False
+jit=False
+torch_compile=False
+optimum_intel=False
+
+# arg parser
 usage() {
  echo "Usage: $0 [OPTIONS]"
  echo "Options:"
  echo " -h, --help            Display this help message"
  echo " -t, --task            Specify task name"
- echo " -m, --model_id        Specify model ID "
- echo " -i, --ipex            Use ipex optimize "
- echo " -j, --jit             Use jit "
- echo " -c, --torch_compile   Use torch compile"
+ echo " -m, --model_id        Specify model id"
  echo " --model_dtype         Indicate the model dtype[float32, bfloat16, float16]"
  echo " --autocast_dtype      Indicate the compute dtype[float32, bfloat16, float16]"
- echo " --backend             Indicate the torch compile backend[ipex, inductor]"
- echo " --device              Indicate the computation device[cpu, cuda, xpu]"
  echo " --batch_size          Input batch size for text-generation"
  echo " --num_beams           The num_beams for text-generation"
  echo " --input_tokens        The input token length for text-generation[32, 64, 128, 256, 512, 1024]"
  echo " --output_tokens       The output token length for text-generation"
- echo " --ipex_optimize_transformers              Ipex optimize_transformers for text-generation"
- echo " --gradient_checkpointing         Whether to run fine-tuning with gradient checkpoint to save memory, only used for fine-tune task"
+ echo " --gradient_checkpointing  Whether to run fine-tuning with gradient checkpoint to save memory, only used for fine-tune task"
+ echo " --quant_algo          Use quant_algo to decide quantization method, options are ["bitsandbytes", "autoawq"]"
+ echo " --quant_dtype         Use quant_dtype to decide quantization data type, like ["int8", "nf4", "fp4"] in bitsandbytes, ["int4"] in autoawq"
+ echo " --backend             Indicate the torch compile backend[ipex, inductor]"
+ echo " --device              Indicate the computation device[cpu, cuda, xpu]"
  echo " --num_processes       The number of data parallelism, only used for CPU fine-tune task"
  echo " --warm_up_steps       The benchmark warm up steps for all tasks"
  echo " --run_steps           The benchmark run steps for all tasks"
+ echo " -i, --ipex            Use ipex optimize"
+ echo " -j, --jit             Use jit"
+ echo " -c, --torch_compile   Use torch compile"
+ echo " --ipex_optimize_transformers              Ipex optimize_transformers for text-generation"
  echo " --optimum_intel       Use optimum-intel optimization"
- echo " --quant_algo          Use quant_algo to decide quantization method, chose from ["bitsandbytes", "autoawq"]"
- echo " --quant_dtype         Use quant_dtype to decide quantization data type, like ["int8", "nf4", "fp4"] in bitsandbytes, ["int4"] in autoawq"
 }
 
 has_argument() {
