85766f579e043154a7768e840e93b9fa1cf13458
Lin, Fanli <fanli.lin@intel.com>
2024-12-10 18:30:07 -0800

    use local folder

--- tests/ut/run_ut.sh
+++ tests/ut/run_ut.sh
@@ -1,13 +1,13 @@
 device="$1"
 target="$2"
 excel_dir=$(dirname "$report")
+result_dir="${excel_dir}/test_results" 
+
+mkdir -p $result_dir
 
 if [[ $target == "transformers" ]]; then
-	export RUN_SLOW=1
 	export RUN_PT_TF_CROSS_TESTS="False"
 	export RUN_PT_FLAX_CROSS_TESTS="False"
-	export TRANSFORMERS_TEST_DEVICE="${device}"
-	export TRANSFORMERS_TEST_DEVICE_SPEC="spec_${device}.py"
 	export WANDB_DISABLED="true"
 
 	# only skip tests that are cpu-only, tpu-only, npu-only, sagemaker-only and tf-only.
@@ -19,16 +19,16 @@ if [[ $target == "transformers" ]]; then
 	cd $target 
 
 	echo "+++++++++run single test files++++++++++++++++"
-	pytest tests/*.py -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --collectonly -q 2>&1 | tee "${excel_dir}/single_files.txt"
-	pytest tests/*.py -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${excel_dir}/single_files.xlsx" --make-reports="${excel_dir}/single_files" --timeout=600
+	pytest tests/*.py -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --collectonly -q 2>&1 | tee "${result_dir}/single_files.txt"
+	pytest tests/*.py -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${result_dir}/single_files.xlsx" --make-reports="${result_dir}/single_files" --timeout=600
 	echo "+++++++++++++++++++++++done for single_files+++++++++++++"
 
 	test_folders=("benchmark" "extended" "fsdp" "generation" "peft_integration" "quantization" "trainer" "pipelines" "deepspeed")
 	for folder in "${test_folders[@]}"
 	do 
 		echo "+++++++++run test folder $folder++++++++++++++++"
-		pytest tests/$folder -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --collectonly -q 2>&1 | tee "${excel_dir}/${folder}.txt"
-		pytest tests/$folder -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${excel_dir}/${folder}.xlsx" --make-reports="${excel_dir}/${folder}" --timeout=600
+		pytest tests/$folder -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --collectonly -q 2>&1 | tee "${result_dir}/${folder}.txt"
+		pytest tests/$folder -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${result_dir}/${folder}.xlsx" --make-reports="${result_dir}/${folder}" --timeout=600
 		echo "+++++++++++++++++++++++done for $folder+++++++++++++"
 	done 
 
@@ -36,13 +36,13 @@ if [[ $target == "transformers" ]]; then
 	do 
 		echo "++++++++run models beginning with $x++++++++++++++++"
 		pytest tests/models/${x}* -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --collectonly -q 2>&1 | tee "${excel_dir}/${x}_models.txt"
-		pytest tests/models/${x}* -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${excel_dir}/${x}_models.xlsx" --make-reports="${excel_dir}/${x}_models" --timeout=600
+		pytest tests/models/${x}* -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${result_dir}/${x}_models.xlsx" --make-reports="${result_dir}/${x}_models" --timeout=600
 		echo "+++++++++++++++++++++++done for $x+++++++++++++"
 	done
 
 	# values=("c")
 	# for x in "${values[@]}"; do
-	#     python -m pytest tests/models/${x}* -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${excel_dir}/${x}_models.xlsx" --make-reports="${excel_dir}/${x}_models" --timeout=600
+	#     python -m pytest tests/models/${x}* -m "${NOT_RUN_MARKERS}" -k "${NOT_RUN_KEYWORDS}" --ignore tests/sagemaker --ignore tests/bettertransformer --excelreport="${result_dir}/${x}_models.xlsx" --make-reports="${result_dir}/${x}_models" --timeout=600
 	#     echo "============done for $x============="
 	# done 
 
